f437c4bb366416823eb3d9679d51ab6dce9448d0
Rodrigo Bonatti <bonatti@Rodrigos-MacBook-Pro.local>
2024-01-22 16:20:39 -0300

    Create delete route

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def update_task(id):
   for t in tasks:
     if t.id == id:
       task = t
+      break
 
   if task == None:
     return jsonify({"message": "Não foi possível encontrar a tarefa"}), 404
@@ -47,6 +48,20 @@ def update_task(id):
   task.description = data["description"]
   task.completed = data["completed"]
   return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route("/tasks/<int:id>", methods=["DELETE"])
+def delete_task(id):
+  task = None
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+
+  if task == None:
+    return jsonify({"message": "Não foi possível encontrar a tarefa"}), 404
+
+  tasks.remove(task)
+  return jsonify({"message": "Tarefa deletada com sucesso"})
 
 if __name__ == "__main__":
   app.run(debug=True)
